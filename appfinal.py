6b051d42417689f7efeedb89712b5d134c704f7d
Navnathgarad <100698159+Navnathgarad@users.noreply.github.com>
2022-09-11 19:50:05 +0530

    Update appfinal.py

--- appfinal.py
+++ appfinal.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pickle
 import numpy
 
-decision_tree_model = pickle.load(open('lr_dtree.pkl','rb'))
+decision_tree_model = pickle.load(open('lr_dtree_2.pkl','rb'))
 #st.title(" Mobile_Price_Range_Prediction ")
 html_temp = """
 <div style="background-color:tomato;padding:10px">
@@ -17,57 +17,50 @@ st.image(image,'mobile')
 
 battery_power= st.slider("Battery Backup(mAH)",501,1998,key="1")
 
-col1, col2 ,col3= st.columns(3)
+col1, col2 = st.columns(2)
 with col1:
     X = st.selectbox("Has 4G Connectivity or Not?",('Yes','No'),key="2")
     trial = ["Yes","No"]
     four_g = trial.index(X)
 
 with col2:
-    Y = st.selectbox("Bluetooth", ('Yes', 'No'), key="3")
-    trial_1 = ["Yes", "No"]
-    blue = trial_1.index(Y)
-
-with col3:
-    Z = st.selectbox("Dual sim", ('Yes', 'No'), key="4")
+    Y = st.selectbox("Has 3G Connectivity or Not?", ('Yes', 'No'), key="3")
     trial_2 = ["Yes", "No"]
-    dual_sim = trial_2.index(Z)
+    three_g = trial_2.index(Y)
 
 col1, col2 = st.columns(2)
 with col1:
-    P = st.selectbox("Has 3G Connectivity or Not?", ('Yes', 'No'), key="5")
-    trial_4 = ["Yes", "No"]
-    three_g = trial_4.index(P)
+    Z = st.selectbox("Touch Screen", ('Yes', 'No'), key="4")
+    trial_3 = ["Yes", "No"]
+    touch_screen = trial_3.index(Z)
 
 with col2:
-    Q = st.selectbox("Wifi Connectivity", ('Yes', 'No'), key="6")
-    trial_5 = ["Yes", "No"]
-    wifi = trial_5.index(X)
+    m_dep = st.selectbox("Mobile Depth(cm)", (0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1), key="5")
 
-int_memory= st.slider("Internal Memory (GB)",2,64,key="7")
+talk_time = st.slider("Battery Last long(Hour)", 2, 20, key="6")
 
 col1, col2 = st.columns(2)
 with col1:
-    pc = st.slider("Primary Camera(MP)",0,20,key="8")
+    pc = st.slider("Primary Camera(MP)",0,20,key="7")
 with col2:
-    fc = st.slider("Front Camera(MP)",0,19,key="9")
+    fc = st.slider("Front Camera(MP)",0,19,key="8")
 
-ram = st.slider("Ram (Mhz i.e 1GB = 1000 Mhz)",256,3998,key="10")
+ram = st.slider("Ram (Mhz i.e 1GB = 1000 Mhz)",256,3998,key="9")
 
 col1, col2 = st.columns(2)
 with col1:
-    px_height = st.slider("Pixel Height(cm)",0,1960,key="11")
+    px_height = st.slider("Pixel Height(cm)",0,1960,key="10")
 with col2:
-    px_width = st.slider("Pixel width(cm)",500,1998,key="12")
+    px_width = st.slider("Pixel width(cm)",500,1998,key="11")
 
 col1, col2 = st.columns(2)
 with col1:
-    sc_h = st.slider("Screen Height(cm)",5,19,key="13")
+    mobile_wt = st.slider("Mobile Weight",80,200,key="12")
 with col2:
-    sc_w = st.slider("Screen width(cm)",5,18,key="14")
+    sc_w = st.slider("Screen width(cm)",0,18,key="13")
 
-def predict_mobile(battery_power,four_g,blue,dual_sim,three_g,wifi,int_memory,pc,fc,ram,px_height,px_width,sc_h,sc_w):
-    input = np.array([[battery_power,four_g,blue,dual_sim,three_g,wifi,int_memory,pc,fc,ram,px_height,px_width,sc_h,sc_w]])#.astype(np.float64)
+def predict_mobile(battery_power,four_g,three_g,touch_screen,m_dep,talk_time,pc,fc,ram,px_height,px_width,mobile_wt,sc_w):
+    input = np.array([[battery_power,four_g,three_g,touch_screen,m_dep,talk_time,pc,fc,ram,px_height,px_width,mobile_wt,sc_w]])#.astype(np.float64)
     prediction = decision_tree_model.predict(input)
     if prediction == 0:
         prediction = "5,000 to 10,000"
@@ -81,7 +74,6 @@ def predict_mobile(battery_power,four_g,blue,dual_sim,three_g,wifi,int_memory,pc
     return prediction
     #return prediction[0]
 if st.button("Predict"):
-    output=predict_mobile(battery_power,four_g,blue,dual_sim,three_g,wifi,int_memory,pc,fc,ram,px_height,px_width,sc_h,sc_w)#.astype(np.int)
+    output=predict_mobile(battery_power,four_g,three_g,touch_screen,m_dep,talk_time,pc,fc,ram,px_height,px_width,mobile_wt,sc_w)#.astype(np.int)
     st.success('The Price of your Mobile must range between {}'.format(output))
-
 
